18cc45f7ab2033f3adf89d9e31f6fe605f3a83c9
alw218 <amberw95@gmail.com>
2016-03-10 04:31:33 -0500

    Updates for prototype submission
    has a few values such as system password and cashier IDs hardcoded in.

--- welcome.java
+++ welcome.java
@@ -9,11 +9,10 @@ public class welcome{
     	cashierIDs.add("2b");
     	cashierIDs.add("3c");
     	cashierIDs.add("4d");
-	String correctPass = "dragonPassword";
+		String correctPass = "dragonPassword";
 		System.out.printf("Welcome to DragonPOS. Please enter system password: ");
 		Scanner scanner = new Scanner(System.in);
 		String systemPass = scanner.next();
-		System.out.println(systemPass);
 		if(!systemPass.equals(correctPass)){
 		    System.out.println("Incorrect system password. Goodbye!");
 		    System.exit(1);
@@ -29,9 +28,16 @@ public class welcome{
 		System.out.println("Welcome. Would you like to start a new transaction? (yes or no): ");
 		String newTransaction = scanner.next();
 		while(newTransaction.equals("yes")){
-			Transaction curTransaction = new Transaction();
-
-
+			SalesTransaction curTransaction = new SalesTransaction();
+			System.out.println("Please enter itemID (if there are no more items enter 'done'): ");
+			String itemID = scanner.next();
+			while(!itemID.equals("done")){
+				curTransaction.addItemByID(itemID);
+				System.out.println("Please enter itemID (if there are no more items enter 'done'): ");
+				itemID = scanner.next();
+			}
+			curTransaction.getPayment(scanner);
+			curTransaction.printReceipt();
 			System.out.println("Would you like to start a new transaction? (yes or no): ");
 			newTransaction = scanner.next();
 		}
