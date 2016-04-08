be3ec93f53fe92b8ce643c17eaf93f2307ced778
xombiesaur <dchaseb23@gmail.com>
2016-04-08 18:29:02 -0400

    updated inventory with conenction shorthand

--- Inventory.java
+++ Inventory.java
@@ -9,33 +9,11 @@ import java.math.* ; // for BigDecimal and BigInteger support
 import java.util.List;
 
 public class Inventory {
-	// JDBC driver name and database URL
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://54.209.1.181/216pos";
-
-	//	Database credentials
-	static final String USER = "amber";
-	static final String PASS = "216216";
 
 	private Connection conn = null;
 
 	public Inventory(){
-		try{
-		//STEP 2: Register JDBC driver
-		Class.forName("com.mysql.jdbc.Driver");
-
-		//STEP 3: Open a connection
-		System.out.println("Connecting to database...");
-		conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-	}
-		catch(SQLException se){
-			//Handle errors for JDBC
-			se.printStackTrace();
-		}catch(Exception e){
-			//Handle errors for Class.forName
-			e.printStackTrace();
-		}
+		conn = DBConnection.getInstance();
 	}//end instantiate
 
 	public String[] getSaleItemFromID(String itemID){
